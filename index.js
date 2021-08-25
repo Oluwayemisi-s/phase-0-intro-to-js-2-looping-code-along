f7cfcb373f5b71a0af5ff58ed566734068807442
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-08-25 09:20:49 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const names = ['Lad', 'Yemisi', 'Jemma', 'Sophie', 'Ade']
+
+function writeCards(names, event){
+   const message = []
+    for (let i = 0; i < names.length; i++){ 
+        message[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        debugger;
+    }
+    return message;
+}
+
+writeCards (names,'wedding');
+
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num)
+        num--
+    }
+}
+
+countDown (10)
+
